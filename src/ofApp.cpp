535007fd9fe9f45436c83a5bbbf5cb188278746e
Meletis Stathis <Meletis@Meletiss-MacBook-Pro.local>
2014-06-22 21:38:26 +0300

    Add shaders and basic code from https://sites.google.com/site/ofauckland/examples/alphablending-shader.

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -1,18 +1,54 @@
 #include "ofApp.h"
-
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    
+    //setup window
+    ofSetWindowShape(500,500);
+    ofSetWindowPosition(0,0);
+    ofBackground(0);
+    
+    ofSetFrameRate(20);
+    
+    //load textures with normalized texcoords (0..1)
+    ofDisableArbTex();
+    earth.loadImage("earth2k.jpg");
+    clouds.loadImage("clouds.jpg");
+    
+    //load the vertex and fragment shaders
+    shader.load("shaders/blend");
+    
+    //initialize sphere
+    quadric = gluNewQuadric();
+    gluQuadricTexture(quadric, GL_TRUE);
+    gluQuadricNormals(quadric, GLU_SMOOTH);
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
+    
 
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    
+    //start camera and flip y-axis
+    cam.begin();
+    ofScale(1,-1);
+    
+    //start shader en set uniform variables
+    shader.begin();
+    shader.setUniformTexture("earth", earth, 1);
+    shader.setUniformTexture("clouds", clouds, 2);
+    shader.setUniform1f("blendAlpha", sin(ofGetElapsedTimef())/2+.5); //fade clouds
+    
+    //draw sphere
+    glEnable(GL_DEPTH_TEST);
+    gluSphere(quadric, 150, 400, 400);
+    
+    //that's all
+    shader.end();
+    cam.end();
 }
 
 //--------------------------------------------------------------
--- src/ofApp.h
+++ src/ofApp.h
@@ -18,5 +18,10 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
+    ofShader shader;
+    ofImage earth,clouds;
+    ofEasyCam cam;
+    GLUquadricObj *quadric; //for gluSphere
 		
 };
